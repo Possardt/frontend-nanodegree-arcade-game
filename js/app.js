7ac3341676052dff972ae1aba01473249b725083
Ryan Possardt <rpossardt@gmail.com>
2017-03-21 21:25:33 -0400

    player movement

--- js/app.js
+++ js/app.js
@@ -24,12 +24,45 @@ Enemy.prototype.render = function() {
 // Now write your own player class
 // This class requires an update(), render() and
 // a handleInput() method.
+var Player = function() {
+    this.sprite = 'images/char-boy.png'
+    this.x = 100;
+    this.y = 100;
+};
+
+Player.prototype.update = function(){
+
+};
+Player.prototype.render = function(){
+    ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
+};
+Player.prototype.handleInput = function(key){
+    if(key === undefined)return;
+    console.log(key);
+    switch(key){
+        case "left":
+            this.x = this.x - 100;
+        break;
+        case "right":
+            this.x = this.x + 100;
+        break;
+        case "up":
+            this.y = this.y - 100;
+        break;
+        case "down":
+            this.y = this.y + 100;
+        break;
+    }
+
+};
 
 
 // Now instantiate your objects.
 // Place all enemy objects in an array called allEnemies
 // Place the player object in a variable called player
-
+var enemy1 = new Enemy();
+var allEnemies = [enemy1];
+var player = new Player();
 
 
 // This listens for key presses and sends the keys to your
